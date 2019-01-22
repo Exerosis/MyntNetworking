8afce8b86d27e278b5da933b6444ae989a6eb18a
TheExerosisGaming <theexerosisgaming@gmail.com>
2019-01-22 14:57:38 -0600

    ~Temporarily patched gradle build.

--- JVM/src/main/kotlin/app/mynt/networking/Test.kt
+++ JVM/src/main/kotlin/app/mynt/networking/Test.kt
@@ -4,21 +4,25 @@ import app.mynt.networking.nio.TCPSocketProvider
 import kotlinx.coroutines.experimental.launch
 import java.net.InetSocketAddress
 import java.nio.ByteBuffer
+import java.nio.ByteOrder
 import java.nio.channels.AsynchronousChannelGroup.withThreadPool
 import java.util.concurrent.Executors.newCachedThreadPool
+import java.util.concurrent.TimeUnit
+
+
 
 fun main(args: Array<String>) {
+    val group = withThreadPool(newCachedThreadPool())
     launch {
-        val provider = TCPSocketProvider(withThreadPool(newCachedThreadPool())) {
-            ByteBuffer.allocate(256).flip() as ByteBuffer
+        val provider = TCPSocketProvider(group) {
+            ByteBuffer.allocate(256).order(ByteOrder.LITTLE_ENDIAN).flip() as ByteBuffer
         }
-
         val address = InetSocketAddress("localhost", 25565)
-        while (provider.isOpen)
-            provider.accept(address).apply {
-                while (isOpen)
-                    println("Got: ${read.byte()}")
-            }
+        while (provider.isOpen) {
+            val client = provider.accept(address)
+            while (client.isOpen)
+                println("Got: ${client.read.byte()}")
+        }
     }
-    Thread.sleep(10000000)
+    group.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS)
 }
